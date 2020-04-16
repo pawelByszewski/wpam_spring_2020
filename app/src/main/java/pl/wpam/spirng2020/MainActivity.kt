fd75ec784e1ebdcc5727fba189c2c4a030374b1c
Pawel Byszewski <pawel.byszewski@polidea.com>
2020-04-16 03:14:19 +0200

    pure project

---
+++ app/src/main/java/pl/wpam/spirng2020/MainActivity.kt
@@ -0,0 +1,18 @@
+package pl.wpam.spirng2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.wpam.spirng2020.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/pl/wpam/spirng2020/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pl.wpam.spirng2020.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
